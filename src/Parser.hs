481ccd24e5a79191dcb24470382e766622802c4c
Hadrian Hughes <hadrianhughes@gmail.com>
2022-05-25 17:20:21 +0100

    Set up parsing logic in a Parser module

---
+++ src/Parser.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Parser where
+
+import Ast
+import Lexer
+import Text.Megaparsec
+import Control.Monad.Combinators.Expr
+
+exprP :: Parser Expr
+exprP = parens exprP
+    <|> angles exprP
+    <|> Operation Beat <$ dot
+    <|> Operation Rest <$ comma
+    <|> Operation SubE <$ echar
+    <|> Operation SubAnd <$ plus
+    <|> Operation SubA <$ achar
+    <|> Neg <$> (dash *> int)
+    <|> try (Bar <$> int <* colon *> angles exprP)
+    <|> Bar 4 <$> angles exprP
+    <|> try (Repitition <$> int <*> exprP)
+    <|> Literal <$> int
+
+programP :: Parser Program
+programP = between sc eof (Program <$> exprP)
