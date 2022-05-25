44a0ec283a649be6853fefb0e1beda79a8763417
Hadrian Hughes <hadrianhughes@gmail.com>
2022-05-25 16:03:07 +0100

    Set up lexemes

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -2,6 +2,7 @@
 
 module Lexer where
 
+import Control.Monad (void)
 import Data.Void
 import Data.Text (Text)
 import Text.Megaparsec
@@ -15,3 +16,42 @@ sc = L.space space1 lineCmnt blockCmnt
   where
     lineCmnt = L.skipLineComment "#"
     blockCmnt = L.skipBlockComment "#-" "-#"
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+symbol :: Text -> Parser Text
+symbol = L.symbol sc
+
+parens :: Parser a -> Parser a
+parens = between (symbol "(") (symbol ")")
+
+slashes :: Parser a -> Parser a
+slashes = between (symbol "/") (symbol "/")
+
+angles :: Parser a -> Parser a
+angles = between (symbol "<") (symbol ">")
+
+dot :: Parser ()
+dot = void $ symbol "."
+
+comma :: Parser ()
+comma = void $ symbol ","
+
+plus :: Parser ()
+plus = void $ symbol "+"
+
+echar :: Parser ()
+echar = void $ symbol "e"
+
+achar :: Parser ()
+achar = void $ symbol "a"
+
+colon :: Parser ()
+colon = void $ symbol ":"
+
+int :: Parser Int
+int = lexeme L.decimal
+
+dash :: Parser ()
+dash = void $ symbol "-"
