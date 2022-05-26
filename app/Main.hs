b54cab609760689dfd1ee691fda6e6de7f52c905
Hadrian Hughes <hadrianhughes@gmail.com>
2022-05-26 19:55:30 +0100

    Properly handle command line arguments

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,40 @@
 module Main where
 
+import Options.Applicative
 import Data.Text as T
 import Parser
-import System.Environment
+import Semant
 
-getInput :: IO String
-getInput = do
-    args <- getArgs
-    pure $ parseArgs args
-  where
-    parseArgs ("-i":input:_) = input
-    parseArgs (_:xs) = parseArgs xs
-    parseArgs [] = error "Provide the input as a single argument."
+data Action = Ast | Sast deriving (Eq)
+
+data Options = Options { action :: Action
+                       , input :: String
+                       }
+
+actionP :: Parser Action
+actionP = flag' Ast (long "ast" <> short 'a' <> help "Print the AST")
+      <|> flag' Sast (long "sast" <> short 's' <> help "Print the SAST")
+
+optionsP :: Parser Options
+optionsP = Options <$> actionP <*> strArgument (help "Input")
 
 main :: IO ()
-main = do
-  input <- getInput
+main = runOpts =<< execParser (optionsP `withInfo` infoString)
+  where
+    withInfo opts desc = info (helper <*> opts) $ progDesc desc
+    infoString = "Run the cajon compiler on the given input."
+
+runOpts :: Options -> IO ()
+runOpts (Options action input) = do
   case runParser programP "" (T.pack input) of
-    Right ast -> putStrLn $ show ast
     Left  err -> putStrLn $ "Parse error: " ++ (show err)
+    Right ast -> do
+      if action == Ast
+         then putStrLn $ show ast
+         else
+          case checkProgram ast of
+            Left err   -> putStrLn $ "Semant Error: " ++ (show err)
+            Right sast -> putStrLn $
+              if action == Sast
+                 then show sast
+                 else "Finished."
