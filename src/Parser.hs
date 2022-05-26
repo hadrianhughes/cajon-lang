362fdd57fff67762858616b256f500cd2974b578
Hadrian Hughes <hadrianhughes@gmail.com>
2022-05-26 11:58:20 +0100

    Create a dedicated operation parser

--- src/Parser.hs
+++ src/Parser.hs
@@ -11,17 +11,20 @@ import Lexer
 import Text.Megaparsec
 import Control.Monad.Combinators.Expr
 
+operationP :: Parser Expr
+operationP = Operation Beat <$ dot
+         <|> Operation Rest <$ comma
+         <|> Operation SubE <$ echar
+         <|> Operation SubAnd <$ plus
+         <|> Operation SubA <$ achar
+
 barP :: Parser Expr
 barP = try (Bar <$> (single '/' *> int) <* colon <*> (Exprs <$> someTill exprP (single '/')))
    <|> Bar 4 <$> Exprs <$> (single '/' *> someTill exprP (single '/'))
 
 exprP :: Parser Expr
 exprP = Exprs <$> parens (some exprP)
-    <|> Operation Beat <$ dot
-    <|> Operation Rest <$ comma
-    <|> Operation SubE <$ echar
-    <|> Operation SubAnd <$ plus
-    <|> Operation SubA <$ achar
+    <|> operationP
     <|> Neg <$> (dash *> int)
     <|> barP
     <|> try (Repitition <$> int <*> exprP)
